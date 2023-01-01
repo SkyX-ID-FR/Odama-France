e52a370d57d084ecca64735e75199d503f445017
SkyX [ID FR] <89273191+SkyX-ID-FR@users.noreply.github.com>
2023-01-01 15:39:03 +0100

    Change contact page..

--- odama-france/pages/contact/index.js
+++ odama-france/pages/contact/index.js
@@ -10,18 +10,6 @@ import Head from 'next/head';
 import Header from '../../components/Header.js';
 
 export default function Contact() {
-    function send_out() {
-        var re_contact_value = document.getElementById("re_contact").value;
-        var message_value = document.getElementById("message_textarea").value;
-
-        if (document.getElementById("message_textarea") && document.getElementById("message_textarea").value) {
-            console.log("Re-contact : " + re_contact_value);
-            console.log("Message : " + message_value);
-        } else {
-            alert("⛔ Vous êtes obligé d'écrire au moins un message pour envoyer votre report ! ⛔");
-        }
-    }
-
     return (
         <>
             <Head>
@@ -34,12 +22,9 @@ export default function Contact() {
 
             <Header/>
             <section id="contact_section">
-                <h1>Un petit message ? 👋</h1>
-                <p>Odama&copy; vous permet d'envoyer au staff toutes demandes, avis ou remarques afin d'améliorez la plateforme avec les avis précieux de tous les utilisateurs. Si vous laissez votre email ou un contact quelconque, nous pourrons vous répondre avec plaisir ! 😊</p>
-            
-                <input placeholder='Écrivez-ici un contact pour que la staff puissent vous répondre (pas obligatoire)...' type="text" id="re_contact"/><br/><br/>
-                <textarea placeholder='Écrivez-ici votre message qui sera envoyé au staff (obligatoire)..' id="message_textarea"/><br/><br/>
-                <button onClick={send_out} id="send_out">OK</button>
+                <a href="" target="_blank"><img className="discord_logo" src='https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6cc3c481a15a141738_icon_clyde_white_RGB.png' alt='discord_logo'/></a>
+                <h1>Odama France <img src='https://images.emojiterra.com/twitter/v13.1/512px/1f1eb-1f1f7.png'/></h1>
+                <p>Odama&copy; est la seule plateforme de streaming 100% cloud, sécurisée et open-source en France. Sans aucunes pubs, des players avec un flux en HD minimum et une interface UI : elle comblera amplement vos souhaits de streaming !</p>
             </section>  
         </>
     )
