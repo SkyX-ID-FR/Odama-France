364cfd4729247c032307d53397d9b6404801a191
SkyX [ID FR] <89273191+SkyX-ID-FR@users.noreply.github.com>
2023-01-18 21:02:24 +0100

    Some adjustements.. 2 ✨

--- odama_france/pages/movies/details/index.js
+++ odama_france/pages/movies/details/index.js
@@ -87,13 +87,14 @@ export default function DetailsMoviesPage(props) {
     document.getElementById("duration").innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg> ${general_movie_data.duration}`;
     document.getElementById("tags").innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tag"><path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"></path><line x1="7" y1="7" x2="7.01" y2="7"></line></svg> ${general_movie_data.tags}`;
     document.getElementById("year").innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg> ${general_movie_data.year}`;
-    document.getElementById("movie_component").innerHTML = `<iframe id="popup_player" src="${general_movie_data.source}" scrolling="no" frameborder="0" allowfullscreen/>`;
+    document.getElementById("movie_component").innerHTML = `<video controls id="popup_player"><source src="${general_movie_data.source}" type="video/mp4"/><video/>`;
   }, []);
 
   function open_movie_popup() { $("#movie_watch_popup").fadeIn(200); }
   function close_movie_popup() { 
     $("#movie_watch_popup").fadeOut(400); 
     $('#popup_player').attr('src', $('#popup_player').attr('src'));
+    $('#popup_player').trigger('pause');
   }
 
   function close_add_movies_toast() { $("#add_movies_toast").fadeOut(500); }
